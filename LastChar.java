88d5b554b2daa872fe88abe00d56d5afb4e3879f
Neeleshmd96 <neeleshd.xworkz@gmail.com>
2022-03-01 22:53:49 +0530

    LastChar.java

---
+++ LastChar.java
@@ -0,0 +1,12 @@
+class LastChar{
+ public static void main(String[] args){
+ 
+  String test="Today is Maha Shivaratri";
+  
+  int num=test.lastIndexOf('a');
+  
+  System.out.println("The last index of the char a in the test string is "+num);
+ 
+ 
+ }
+}
