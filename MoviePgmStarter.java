3436aabb1eb3595e7a9f2bdf7af0a2d7bd62a339
Neeleshmd96 <neeleshd.xworkz@gmail.com>
2022-03-02 00:58:01 +0530

    MoviePgmStarter.java

---
+++ MoviePgmStarter.java
@@ -0,0 +1,38 @@
+class MoviePgmStarter{
+ public static void main(String[] args){
+ 
+    MoviePgm movies=new MoviePgm();
+ 
+    movies.addMovie("Badava Rascal");
+    movies.addMovie("Garuda Gamana");
+    movies.addMovie("Robert");
+    movies.addMovie("Love mocktail");
+    movies.addMovie("Bhajarangi");
+	movies.addMovie("Om");
+    movies.addMovie("Attrangi Re");
+    movies.addMovie("Antim");
+    movies.addMovie("Tadap");
+    movies.addMovie("Sooryavanshi");
+    movies.addMovie("Pushpa");
+	movies.addMovie(null);
+    movies.addMovie("Dune");
+    movies.addMovie("Free Guy");
+    movies.addMovie("Eternals");
+    movies.addMovie("Shangi Chi");
+	movies.addMovie("No Time to Die");
+    movies.addMovie("Venom");
+    movies.addMovie("Jungle Cruise");
+    movies.addMovie("Red notice");
+    movies.addMovie("Bhajarangi 2");
+	movies.addMovie("ABCD 2");
+    movies.addMovie("");
+	movies.addMovie("Akhanda");
+	movies.addMovie("Vakeel Saab");
+   
+    
+	movies.display();
+ 
+ }
+
+
+}
